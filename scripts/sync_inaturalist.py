787a695573ecde63842318c7db7c1f665d5f114d
Ernesto Junior <ernesto.sjunior@hotmail.com>
2026-05-10 20:02:25 -0300

    fix: update place_id in fetch_page function for accurate location filtering in iNaturalist synchronization

--- scripts/sync_inaturalist.py
+++ scripts/sync_inaturalist.py
@@ -56,7 +56,7 @@ def _parse_location(location):
 def _fetch_page(taxon_id, id_above):
     params = {
         "taxon_id": taxon_id,
-        "place_id": 6803, #BR
+        "place_id": 6878, #BR
         "has[]": "geo",
         "per_page": PER_PAGE,
         "order": "asc",
